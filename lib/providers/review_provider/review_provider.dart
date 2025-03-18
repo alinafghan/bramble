312d8bb5f504ac8528af098be5b7eca5c4cec704
Alina <alinanasirafghan@gmail.com>
2025-03-18 22:51:05 +0500

    review provider

---
+++ lib/providers/review_provider/review_provider.dart
@@ -0,0 +1,20 @@
+import 'package:journal_app/models/book.dart';
+import 'package:journal_app/models/review.dart';
+import 'package:journal_app/models/user.dart';
+import 'package:journal_app/repositories/review_repository.dart';
+
+class ReviewProvider {
+  final ReviewRepository reviewRepository = ReviewRepository();
+
+  Future<List<Review?>> getReviewsForBook(Book book) async {
+    return reviewRepository.getReviewsForBook(book);
+  }
+
+  Future<Review?> getReview(Book book, Users user) async {
+    return reviewRepository.getReview(book, user.userId);
+  }
+
+  Future<Review?> setReview(Book book, Review review) async {
+    return reviewRepository.setReview(review, book);
+  }
+}
